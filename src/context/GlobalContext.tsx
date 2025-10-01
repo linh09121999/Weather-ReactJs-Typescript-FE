99dc6518e46a8730a25e2ac22faea32da35eb0a7
linh1999 <linhct020328@gmail.gmail.com>
2025-10-01 13:14:40 +0700

    deploy5

--- src/context/GlobalContext.tsx
+++ src/context/GlobalContext.tsx
@@ -1029,7 +1029,7 @@ export const GlobalProvider = ({ children }: { children: ReactNode }) => {
 
     const checkTimeExp = () => {
         if (!timestamp) return true;
-        return Date.now() - timestamp > 5 * 60 * 1000; //phút
+        return Date.now() - timestamp > 60 * 1000; //phút
     };
 
     const [selectQ, setSelectQ] = useState<string | undefined>("HaNoi");
